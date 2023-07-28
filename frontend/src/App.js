302fbdb770be146a1e95d6a95475880b559f77d9
Drishey Singh <drisheysingh@gmail.com>
2023-07-28 21:36:52 +0530

    login and regiter page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,32 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-
 import HomePage from "./pages/homePage/home";
 import LoginPage from "./pages/loginPage/login";
+import RegisterPage from "./pages/loginPage/register";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 
 function App() {
+
+  const theme = useMemo(() => createTheme(themeSettings('light')));
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-      </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/login' element={<LoginPage/>}/>
+            <Route path='/register' element={<RegisterPage/>}/>
+          </Routes>
+        </ThemeProvider>
+      
       
       </BrowserRouter>
     </div>
