088a036bcaf44463a21edb2ebe0f30f1aeb54dc3
Drishey Singh <drisheysingh@gmail.com>
2023-07-28 17:08:52 +0530

    revised folder struture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
+import HomePage from "./pages/homePage/home";
+import LoginPage from "./pages/loginPage/login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Flipr</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+      </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/loginPage/login.jsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return(
+        <h1>
+            this is login page
+        </h1>
+    )
+}
+
+export default LoginPage;
