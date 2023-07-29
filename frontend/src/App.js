263ca181ec1de6545c5a494b3a5968de36224924
lalwaniprashant <prashantlalwani55232@gmail.com>
2023-07-29 16:10:50 +0530

    p working frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-
-import HomePage from "./pages/homePage/home";
 import LoginPage from "./pages/loginPage/login";
+import RegisterPage from "./pages/loginPage/register";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import Home from "./pages/home/Home";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings("light")));
@@ -11,8 +16,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,47 +4,51 @@ import { Link } from "react-router-dom";
 import Row from "./Row";
 import LandPage from "./LandPage";
 import Review from "./Review";
+import NavBar from "../navbar/NavBar";
 
 const Home = () => {
   return (
-    <div className="home">
-      <LandPage />
-      <div className="guidePage">
-        <div className="heading">
-          <p>Guides for your next vacation</p>
-          <p>
-            Checkout this week's selection of popular products that might catch
-            your
-          </p>
+    <>
+      <NavBar />
+      <div className="home">
+        <LandPage />
+        <div className="guidePage">
+          <div className="heading">
+            <p>Guides for your next vacation</p>
+            <p>
+              Checkout this week's selection of popular products that might
+              catch your
+            </p>
+          </div>
+          <div className="filter">
+            <Link to={"/"} className="filterText">
+              All
+            </Link>
+            <Link to={"/"} className="filterText">
+              Trending
+            </Link>
+            <Link to={"/"} className="filterText">
+              Popular
+            </Link>
+            <Link to={"/"} className="filterText">
+              Feature
+            </Link>
+            <Link to={"/"} className="filterText">
+              Recommended
+            </Link>
+            <Link to={"/"} className="filterText">
+              Tour Packages
+            </Link>
+          </div>
+          <div className="rowSection">
+            <Row />
+            <Row />
+            <Row />
+          </div>
         </div>
-        <div className="filter">
-          <Link to={"/"} className="filterText">
-            All
-          </Link>
-          <Link to={"/"} className="filterText">
-            Trending
-          </Link>
-          <Link to={"/"} className="filterText">
-            Popular
-          </Link>
-          <Link to={"/"} className="filterText">
-            Feature
-          </Link>
-          <Link to={"/"} className="filterText">
-            Recommended
-          </Link>
-          <Link to={"/"} className="filterText">
-            Tour Packages
-          </Link>
-        </div>
-        <div className="rowSection">
-          <Row />
-          <Row />
-          <Row />
-        </div>
+        <Review />
       </div>
-      <Review />
-    </div>
+    </>
   );
 };
 
